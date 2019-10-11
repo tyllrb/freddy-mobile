b9100ce16708094b4d65f4f3bc94976e56db384b
Tyler <tyler.borchert@td.com>
2019-10-11 16:01:14 -0400

    Added links to online react mobile resources that may be useful

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+/************************************************
+ *
+ * This is a react web application designed specifically for mobile
+ * Here is a collection of mobile based react components:
+ *    https://github.com/brillout/awesome-react-components
+ *
+ * Bootstrap theme being used: https://bootswatch.com/lux/
+ */
+
 import React from 'react';
 import Sidebar from './components/sidebar/sidebar';
 import './App.css';
